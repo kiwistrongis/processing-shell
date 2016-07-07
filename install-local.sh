6b49857f518b1529d62e309ee17c960609ebf9cb
kurotetsuka <kurotetsuka@gmail.com>
2016-07-07 14:41:59 -0400

    updated for processing 3.0 ( 3.1.1, actually )

--- install-local.sh
+++ install-local.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-if [[ $(whoami) != "root" ]]; then
-	echo -e "You must be root to install!"
-	exit 1
-fi
-
-target="/usr/local/bin"
+target="~/.local/bin"
 
 install -m 755 processing-shell $target/processing-shell
 ln -sf $target/processing-shell $target/pshell
